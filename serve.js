be528c6f4455f6132852f6bb5439295a9bb38797
Aaron Wright <aaron@sikosoft.com>
2019-11-10 13:10:01 +0100

    renamed core.json to config.js, served & re-routed from /

--- serve.js
+++ serve.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const router = express.Router();
+const fs = require('fs');
 
 const app = express();
 
 const staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
+
+router.get('/config.json', (req, res) => {
+  fs.readFile('./config.json', (error, data) => {
+    res.send(data);
+  });
+});
+app.use(router);
 
 app.listen(3000, function() {
   console.log('listening');
